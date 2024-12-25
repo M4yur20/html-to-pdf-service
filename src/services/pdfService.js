b51d7e714b0e1bcbe5553b19c00a74ad7e261090
Mayur Agarwal <mayuragarwal929@gmail.com>
2024-12-25 13:51:40 +0530

    done

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -6,7 +6,7 @@ class PdfService {
     let browser;
     try {
       browser = await puppeteer.launch({
-        headless: 'new',
+        headless: 'true',
         args: ['--no-sandbox', '--disable-setuid-sandbox']
       });
 
